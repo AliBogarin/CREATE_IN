f13f5c5913050b9f2238aa2153baed073d275270
Alica bogarin <abogarinv@fpcoruna.afundacion.org>
2024-05-13 22:56:06 +0200

    Añadiendo links que serán clickables

--- app/src/main/java/com/example/createinn/MainActivity.java
+++ app/src/main/java/com/example/createinn/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.createinn;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,4 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         }
+
+     @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+         getMenuInflater().inflate(R.menu.main_menu,menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            /* esto va a se en el caso de clicar encima de alguno de ellos*/
+        }
+        return super.onOptionsItemSelected(item);
     }
+}
